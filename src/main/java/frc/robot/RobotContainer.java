a6fa95057966468cddacb4cf943b4f933e01082e
Joseph Argento <joseph.m.argento@gmail.com>
2025-01-18 13:34:49 -0500

    Removed merge tags.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -110,9 +110,7 @@ public class RobotContainer {
 
     private void configurePathPlanner() {
         autoChooser.addOption("Vision Test",   "Vision Test");
-<<<<<<< HEAD
         autoChooser.addOption("Drive Straight",   "Drive Straight");
-=======
         autoChooser.addOption("SwerveTestAuto25", "SwerveTestAuto25");
         autoChooser.addOption("StraightForward", "StraightForward");
     }
