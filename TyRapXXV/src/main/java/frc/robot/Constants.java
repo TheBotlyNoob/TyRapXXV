1fd0e21c92c0ba45eded9a7a95449c7423fb864e
TheBotlyNoob <jj@jjay.dev>
2025-01-28 20:53:22 -0500

    added inital algae tests for demoboard

---
+++ TyRapXXV/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public class Constants {
+
+    public static class ID {
+        /** Algae retriever motor ID. */
+        public static final int kAlgaeGrabberMotorCANID = 11;
+
+        /** Algae retriever solenoid ID. */
+        public static final int kAlgaeGrabberSolenoidCANID1 = 1;
+        public static final int kAlgaeGrabberSolenoidCANID2 = 2;
+    }
+}
---
+++ TyRapXXV/src/main/java/frc/robot/subsystems/AlgaeGrabberSubsystem.java
@@ -0,0 +1,117 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants;
+
+import com.revrobotics.spark.SparkMax;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+
+public class AlgaeGrabberSubsystem extends SubsystemBase {
+  private SparkMax retrieval_motor;
+  private DoubleSolenoid raise_pneumatics_solenoid;
+
+  private ShuffleboardTab shuffle_tab = Shuffleboard.getTab(getName());
+
+  private GenericEntry shuffle_motor_current = shuffle_tab.add("Motor Current", 0.0)
+      .withWidget(BuiltInWidgets.kNumberBar)
+      .withProperties(Map.of("max", 50.0)).withSize(2, 1)
+      .withPosition(0, 0).getEntry();
+
+  private GenericEntry shuffle_motor_voltage = shuffle_tab.add("Motor Voltage", 0.0)
+      .withWidget(BuiltInWidgets.kNumberBar)
+      .withProperties(Map.of("max", 12.0)).withSize(2, 1)
+      .withPosition(0, 1).getEntry();
+
+  // private GenericEntry shuffle_compressor_current = shuffle_tab.add("Compressor
+  // Current Draw", 0.0)
+  // .withWidget(BuiltInWidgets.kNumberBar)
+  // .withSize(2, 1).withPosition(2, 0).getEntry();
+  // private GenericEntry shuffle_compressor_pressure =
+  // shuffle_tab.add("Compressor Pressure", 0.0)
+  // .withWidget(BuiltInWidgets.kNumberBar)
+  // .withSize(2, 1).withPosition(2, 1).getEntry();
+
+  private GenericEntry shuffle_solenoid_state = shuffle_tab.add("Solenoid State", "Off")
+      .withWidget(BuiltInWidgets.kTextView)
+      .withSize(2, 1).withPosition(2, 2).getEntry();
+
+  /** Creates a new AlgaeGrabber. */
+  public AlgaeGrabberSubsystem() {
+    retrieval_motor = new SparkMax(Constants.ID.kAlgaeGrabberMotorCANID, MotorType.kBrushless);
+
+    // FIXME: put in proper solneoid type
+    raise_pneumatics_solenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH,
+        Constants.ID.kAlgaeGrabberSolenoidCANID1, Constants.ID.kAlgaeGrabberSolenoidCANID2);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command beginRetrieval() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          retrieval_motor.set(1.0);
+          raise_pneumatics_solenoid.set(Value.kForward);
+
+          new Trigger(() -> isMotorUnderLoad()).onTrue(runOnce(() -> {
+            retrieval_motor.set(0.0);
+            raise_pneumatics_solenoid.set(Value.kOff);
+          }));
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a
+   * digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  public boolean isMotorUnderLoad() {
+    return retrieval_motor.getOutputCurrent() > 40;
+  }
+
+  @Override
+  public void periodic() {
+    shuffle_motor_current.setDouble(retrieval_motor.getOutputCurrent());
+    shuffle_motor_voltage.setDouble(retrieval_motor.getAppliedOutput());
+
+    // shuffle_compressor_current.setDouble(raise_pneumatics_compressor.getCurrent());
+    // shuffle_compressor_pressure.setDouble(raise_pneumatics_compressor.getPressure());
+
+    shuffle_solenoid_state.setString(raise_pneumatics_solenoid.toString());
+
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
