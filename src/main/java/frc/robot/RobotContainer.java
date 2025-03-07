a71406d46ac6f72fb7f5014a91b5918e7e7e9e9f
jnipar <jnipar311@gmail.com>
2025-03-06 21:14:48 -0500

    modified led subsystem to change when setting elevator levels

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,9 +43,9 @@ import frc.robot.Constants.*;
 import frc.robot.Subsystems.AlgaeGrabberSubsystem;
 import frc.robot.Subsystems.ClimberSubsystem;
 import frc.robot.Subsystems.Drivetrain;
+import frc.robot.Subsystems.ElevatorLED;
 import frc.robot.Subsystems.ElevatorSubsystem;
 import frc.robot.Subsystems.ElevatorSubsystem.ElevatorLevel;
-import frc.robot.Subsystems.AutoScoreLed;
 import frc.robot.Subsystems.Limelight;
 import frc.robot.Subsystems.RangeSensor;
 import frc.robot.Subsystems.CoralSubsystem;
@@ -86,7 +86,7 @@ public class RobotContainer {
     private final SendableChooser<String> autoChooser;
     protected final ElevatorSubsystem m_elevator;
     protected final CoralSubsystem m_coral;
-    protected final AutoScoreLed m_leds;
+    protected final ElevatorLED m_leds;
 
     private ShuffleboardTab m_competitionTab = Shuffleboard.getTab("Competition Tab");
     private GenericEntry m_xVelEntry = m_competitionTab.add("Chassis X Vel", 0).getEntry();
@@ -139,12 +139,7 @@ public class RobotContainer {
         this.m_elevator = new ElevatorSubsystem(NetworkTableInstance.getDefault());
         this.m_coral = new CoralSubsystem(NetworkTableInstance.getDefault());
 
-        AddressableLED led = new AddressableLED(0);
-        led.setLength(5);
-
-        AddressableLEDBuffer ledBuf = new AddressableLEDBuffer(5);
-
-        this.m_leds = new AutoScoreLed(led, ledBuf, m_Limelight, m_coral, m_elevator);
+        this.m_leds = new ElevatorLED(m_elevator);
 
         // Xbox controllers return negative values when we push forward.
         this.m_driveCommand = new Drive(m_swerve);
