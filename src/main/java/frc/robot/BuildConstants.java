abd6a456d470a906649778f735ae12e9a22921e4
TheBotlyNoob <jj@jjay.dev>
2025-04-12 12:25:15 -0400

    add velocity end states

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "TyRapXXV";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 347;
+  public static final String GIT_SHA = "11bad1ac86c8e088ba42a15e20962507ed8bd866";
+  public static final String GIT_DATE = "2025-04-03 22:54:36 EDT";
+  public static final String GIT_BRANCH = "feature/simulation";
+  public static final String BUILD_DATE = "2025-04-10 14:10:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1744308650496L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
